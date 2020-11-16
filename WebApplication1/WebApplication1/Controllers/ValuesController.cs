7c2e043db53f6cd54425ed2108cdfb27489eb909
Alexandru Balan <61700567+Al3xBM@users.noreply.github.com>
2020-11-16 19:02:28 +0200

    ML.NET microservices initial commit

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using WebApplication1ML.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpPost("getPrediction")]
+        public ActionResult<ModelOutput> GetPrediction(ModelInput modelInput)
+        {
+            var predictionResult = ConsumeModel.Predict(modelInput);
+
+            Dictionary<string, string> ret = new Dictionary<string, string>();
+
+            ret.Add("prediction", predictionResult.Prediction);
+            ret.Add("score", predictionResult.Score.ToString());
+            // JObject json = JObject.Parse(ret);
+
+            return predictionResult;
+        }
+    }
+}
