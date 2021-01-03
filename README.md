e7dc21147d67473f8645dc142ca46cbff6044794
mcrisan5537 <crisanmihai5537@gmail.com>
2021-01-03 15:12:24 +0200

    Change application ports

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Fake news detector
 
 
 [Blazor WebAssembly app available on Azure as a Static Web App](https://orange-glacier-067bc8f03.azurestaticapps.net)
+
+
+| Port mappings         | HTTPS | HTTP |
+| --------------------- | ----- |
+| BlazorWebApp          | 5100  | 5099 |
+| ClassificationService | 5200  | 5199 |
+| NewsStorage           | 5300  | 5299 |
+| UserService           | 5400  | 5399 |
---
+++ crawler/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using HtmlAgilityPack.CssSelectors.NetCore;
+
+namespace webCrawler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startCrawlerAsync("Trump Junior TOTALLY Staged This Awkward Photo For The NYT, And Twitter Loves It").GetAwaiter();
+        }
+
+        private static async Task startCrawlerAsync(string title)
+        {
+            char[] delimiters = { ' ', ',', '.', ':', '\t' };
+            string[] title_words = title.Split(delimiters);
+            var url = "http://www.google.com/search?q=";
+            foreach (string word in title_words)
+            {
+                url = url + "+" + word;
+            }
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var divs = htmlDocument.DocumentNode.Descendants("div")
+                        .Where(node => node.GetAttributeValue("id", "").Equals("main")).ToList();
+
+            var pagesUrls = new List<HtmlNode>();
+            for (int i = 14; i < 24; ++i)
+            {
+                pagesUrls.Add(divs[0].Descendants("a").ElementAt(i));
+            }
+
+            List<string> actualUrls = new List<string>();
+            List<string> titles = new List<string>();
+            foreach (var page in pagesUrls)
+            {
+                var page_link = page.GetAttributeValue("href", "");
+                page_link = page_link.Remove(0, 7);
+                var ind = page_link.IndexOf("&amp");
+                page_link = page_link.Remove(ind, page_link.Length - ind);
+
+                actualUrls.Add(page_link);
+
+                titles.Add(page.Descendants("h3").ToList()[0].InnerText);
+            }
+
+            // Environment.Exit(0);
+        }
+    }
+}
